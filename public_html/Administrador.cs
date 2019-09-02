f9453287cf47f573009506b68bd9aaeac807cb0a
carlos eduardo hayashi <duduhayjoga@gmail.com>
2019-09-02 18:38:49 -0300

    modifiquei o arquvo index.html

---
+++ public_html/Administrador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlameOffice_Carlos_1705971.Classes
+{
+    class Administrador : Usuario
+    {
+        public int ID_adm { get; set; }
+    }
+}
+
